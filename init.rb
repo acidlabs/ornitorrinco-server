03fbf379744d45ee8dde35749aa3a463ef04379e
maukoquiroga <mauko@pictorical.com>
2011-10-07 19:16:16 -0300

    hey

---
+++ init.rb
@@ -0,0 +1,27 @@
+require 'yajl/json_gem'
+
+module Ornitorrinco  
+  class Init < Sinatra::Base
+    
+    configure do
+      env = ENV['SINATRA_ENV'] || 'development'
+      GEOIP = GeoIP.new('config/GeoLiteCity.dat')
+    end
+    
+    mime_type :json, 'application/json'
+    
+    before do
+      content_type :json
+    end
+    
+    get '/' do
+      begin
+        response = GEOIP.city(request[:ip])
+        response ? response.to_json : raise(Error)
+      rescue
+        "City not found".to_json
+      end
+    end
+  
+  end
+end
