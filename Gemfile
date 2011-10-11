741030053983312e2d254beebef704bff3a3700d
maukoquiroga <mauko@pictorical.com>
2011-10-11 16:24:14 -0300

    yup

--- Gemfile
+++ Gemfile
@@ -3,7 +3,6 @@ source :rubygems
 gem 'yajl-ruby'
 gem 'sinatra'
 gem 'geoip'
-gem 'redis-store'
 
 group :development do
   gem 'kumade'
--- init.rb
+++ init.rb
@@ -2,12 +2,9 @@ require 'yajl/json_gem'
 
 module Ornitorrinco  
   class Init < Sinatra::Base
-    register Sinatra::Cache
-    
+
     configure do
       env = ENV['SINATRA_ENV'] || 'development'
-      uri = URI.parse(ENV["REDISTOGO_URL"])
-      REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
       GEOIP = GeoIP.new('config/GeoLiteCity.dat')
     end
     
@@ -17,11 +14,14 @@ module Ornitorrinco
       content_type :json
       headers['Cache-Control'] = "public; max-age=#{365*24*60*60}"
     end
-    
-    get '/' do
-      response = GEOIP.city request.ip
-      response = response ? { :status => 'ok', :city => response.to_hash[:city_name] }.to_json : { :status => 'not found', :message => "City not found" }.to_json
+        
+    get '/location/:ip' do
+      begin
+        response = GEOIP.city params[:ip]
+        response = response ? { :status => 'ok', :city => response.to_hash[:city_name] }.to_json : { :status => 'not found', :message => "City not found" }.to_json
+      rescue => e
+        error 500, e.message.to_json
+      end
     end
-  
   end
 end
